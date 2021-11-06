372f1b12cc71ced09f07478fa7d11491dc39c14a
Samraui <93262622+Samraui@users.noreply.github.com>
2021-11-06 19:58:23 +0800

    第一次提交

---
+++ src.com.java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Sun Lei
+ * @Date: Created in 20:13 2021/11/6
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello sun");
+    }
+}
